0cb18230e0a37bdb7887d05cc10a05ad161d1d58
Milindh Vijay <milindhvijay@gmail.com>
2022-09-04 21:46:46 +0530

    restructure into function

--- aftership/lastcheckpoint.py
+++ aftership/lastcheckpoint.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from dotenv import load_dotenv
-from register import tracking_id
+from register import register_func
 
 load_dotenv()
 
@@ -10,7 +10,23 @@ API_KEY = os.getenv('AFTERSHIP_API_KEY')
 
 id = tracking_id
 
-headers = {
+def last_checkpoint_func(message):
+    headers = {
+        'Content-Type': 'application/json',
+        'as-api-key': API_KEY,
+    }
+
+    response = requests.get(
+        'https://api.aftership.com/v4/last_checkpoint/%s' % id, headers=headers)
+
+    last_checkpoint_data = response.json()
+
+    city = last_checkpoint_data['data']['checkpoint']['city']
+    message = last_checkpoint_data['data']['checkpoint']['message']
+
+    return message
+
+"""headers = {
     'Content-Type': 'application/json',
     'as-api-key': API_KEY,
 }
@@ -25,4 +41,4 @@ message = last_checkpoint_data['data']['checkpoint']['message']
 
 
 print(message)
-print(city)
+print(city)"""
