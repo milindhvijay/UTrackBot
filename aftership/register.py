96ac2fbd580ca4948add89ff43be8778defced55
Milindh Vijay <milindhvijay@gmail.com>
2022-09-04 21:46:34 +0530

    restructured in function

--- aftership/register.py
+++ aftership/register.py
@@ -1,3 +1,4 @@
+from csv import register_dialect
 import os
 import requests
 from dotenv import load_dotenv
@@ -6,23 +7,23 @@ load_dotenv()
 
 API_KEY = os.getenv('AFTERSHIP_API_KEY')
 
-headers = {
-    'as-api-key': API_KEY,
-}
 
-json_data = {
-    'tracking': {
-        'tracking_number': 'RR123456789CN',
-    },
-}
+def register_func(tracking_id):
+    headers = {
+        'as-api-key': API_KEY,
+    }
 
-response = requests.post(
-    'https://api.aftership.com/v4/trackings', headers=headers, json=json_data)
+    json_data = {
+        'tracking': {
+            'tracking_number': "RR123456789CN",
+        },
+    }
 
-# print(response.text)
+    response = requests.post(
+        'https://api.aftership.com/v4/trackings', headers=headers, json=json_data)
 
-register_data = response.json()
+    register_data = response.json()
 
-tracking_id = register_data['data']['tracking']['id']
+    tracking_id = register_data['data']['tracking']['id']
 
-# print(tracking_id)
+    return tracking_id
