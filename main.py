e11a13819fe5b4c19387f64d6b586f7655993738
Milindh Vijay <milindhvijay@gmail.com>
2022-09-05 12:02:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-from email import message
-import os
-import types
-import telebot
-from telebot import types
-from buttonmenu import *
-from dotenv import load_dotenv
-from aftership.lastcheckpoint import last_checkpoint_func
 from greeting import greetings
 from tracking import trackings
-
-load_dotenv()
-
-BOT_API_KEY = os.getenv('BOT_API_KEY')
-
-bot = telebot.TeleBot(BOT_API_KEY)
-
 
 # Greeting Message
 greetings()
