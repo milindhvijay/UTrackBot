cbd87bb8c0b1e37aed1938d1351b66a62857ee13
Milindh Vijay <milindhvijay@gmail.com>
2022-09-05 12:00:16 +0530

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -26,4 +26,3 @@ def greetings():
         bot.send_message(message.chat.id, "👋 Hi " + message.from_user.first_name +
                      ", my name is UTrackBot and I can help you keep track of your shipments.")
         bot.send_message(message.chat.id, "✅ What you need to know:\n\n1️⃣ This service is free and unlimited.\n\n2️⃣ To add a new shipment, tap ➕ *Track* down here, or send the tracking number directly.\n\n3️⃣ For help if you face a problem, tap ❓ *Help*.", parse_mode='Markdown', reply_markup=markup)
-    #bot.send_message(message.from_user.id, "*hello*", parse_mode='Markdown')
